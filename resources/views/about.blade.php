3cfee9865abd96346eed91b55efa0f6d8c94c1c8
virgi-septian <virgiseptian77@gmail.com>
2022-07-10 19:56:07 +0700

    initial commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>WPU BLOG | About</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <h1>Halaman About</h1>
+
+    <ul>
+        <li>Nama = <?= $nama ?></li>
+        <li>Email = <?= $email ?></li>
+        <li>Kelas = <?= $kelas ?></li>
+        <li><img src="img/<?= $img ?>" alt="" height="270" width="235"></li>
+    </ul>
+</body>
+
+</html>
