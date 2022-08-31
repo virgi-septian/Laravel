6e90b7d36fd734284db08a9770c0eb76d9ccf731
virgi-septian <virgiseptian77@gmail.com>
2022-08-31 21:52:49 +0700

    Seeder sederhana

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Post;
+use App\Models\Category;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,11 +17,47 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        User::create(
+            [
+                'name' => 'Virgi Septian',
+                'email' => 'virgiseptian77@gmail.com',
+                'password' => bcrypt('123')
+            ]
+        );
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        Category::create([
+            'name' => 'Web Programming' , 
+            'slug' => 'web-programming'
+        ]);
+        Category::create([
+            'name' => 'Personal' , 
+            'slug' => 'personal',
+        ]);
+
+        Post::create([
+            'title' => 'Judul Post Pertama', 
+            'slug' => 'judul-post-pertama', 'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt nobis tenetur et similique sit, quisquam esse illo dolores eos placeat culpa ab ratione accusamus repudiandae quibusdam sed voluptas fuga consequuntur laboriosam molestiae nesciunt nemo nisi ut.', 
+            'body' => '<p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt nobis tenetur et similique sit, quisquam esse illo dolores eos placeat culpa ab ratione accusamus repudiandae quibusdam sed voluptas fuga consequuntur laboriosam molestiae nesciunt nemo nisi ut. Repellat, illo officiis perferendis rerum consequatur ipsam natus accusamus animi voluptatem in vel iure. Error mollitia deserunt eos. Non fugiat officiis neque cupiditate cumque autem rem hic repudiandae quia omnis eos dicta quos pariatur doloribus nemo libero est, delectus, quaerat soluta voluptate mollitia nesciunt et ipsa facere! Tempore in adipisci</p> <p>nesciunt possimus ipsam doloribus vero enim, dolorem nemo iusto sed alias saepe soluta ducimus minus sint architecto eaque accusamus exercitationem quidem necessitatibus asperiores! Temporibus sapiente molestias eum illum itaque autem nostrum debitis officia quis? Quia animi cum, dicta doloremque architecto vel recusandae deleniti adipisci asperiores odio dignissimos veritatis voluptatem libero nulla sunt possimus. Harum aperiam inventore minima consequuntur distinctio corporis iure deserunt officia incidunt?</p> <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quis veniam quod nobis temporibus consequatur incidunt, impedit maxime totam minus officiis quia eveniet sed reprehenderit, nostrum ut fugit ipsam iusto accusantium tempora est. Laudantium suscipit sit doloribus dolore tenetur! Sapiente, ab?</p>',
+            'category_id' => 1, 
+            'user_id' => 1 
+        ]);
+         
+        Post::create([
+            'title' => 'Judul Post Kedua', 
+            'slug' => 'judul-post-kedua', 'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt nobis tenetur et similique sit, quisquam esse illo dolores eos placeat culpa ab ratione accusamus repudiandae quibusdam sed voluptas fuga consequuntur laboriosam molestiae nesciunt nemo nisi ut.', 
+            'body' => '<p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt nobis tenetur et similique sit, quisquam esse illo dolores eos placeat culpa ab ratione accusamus repudiandae quibusdam sed voluptas fuga consequuntur laboriosam molestiae nesciunt nemo nisi ut. Repellat, illo officiis perferendis rerum consequatur ipsam natus accusamus animi voluptatem in vel iure. Error mollitia deserunt eos. Non fugiat officiis neque cupiditate cumque autem rem hic repudiandae quia omnis eos dicta quos pariatur doloribus nemo libero est, delectus, quaerat soluta voluptate mollitia nesciunt et ipsa facere! Tempore in adipisci</p> <p>nesciunt possimus ipsam doloribus vero enim, dolorem nemo iusto sed alias saepe soluta ducimus minus sint architecto eaque accusamus exercitationem quidem necessitatibus asperiores! Temporibus sapiente molestias eum illum itaque autem nostrum debitis officia quis? Quia animi cum, dicta doloremque architecto vel recusandae deleniti adipisci asperiores odio dignissimos veritatis voluptatem libero nulla sunt possimus. Harum aperiam inventore minima consequuntur distinctio corporis iure deserunt officia incidunt?</p> <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quis veniam quod nobis temporibus consequatur incidunt, impedit maxime totam minus officiis quia eveniet sed reprehenderit, nostrum ut fugit ipsam iusto accusantium tempora est. Laudantium suscipit sit doloribus dolore tenetur! Sapiente, ab?</p>',
+            'category_id' => 1, 
+            'user_id' => 1 
+        ]);
+         
+        Post::create([
+            'title' => 'Judul Post Ketiga', 
+            'slug' => 'judul-post-ketiga', 'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt nobis tenetur et similique sit, quisquam esse illo dolores eos placeat culpa ab ratione accusamus repudiandae quibusdam sed voluptas fuga consequuntur laboriosam molestiae nesciunt nemo nisi ut.', 
+            'body' => '<p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt nobis tenetur et similique sit, quisquam esse illo dolores eos placeat culpa ab ratione accusamus repudiandae quibusdam sed voluptas fuga consequuntur laboriosam molestiae nesciunt nemo nisi ut. Repellat, illo officiis perferendis rerum consequatur ipsam natus accusamus animi voluptatem in vel iure. Error mollitia deserunt eos. Non fugiat officiis neque cupiditate cumque autem rem hic repudiandae quia omnis eos dicta quos pariatur doloribus nemo libero est, delectus, quaerat soluta voluptate mollitia nesciunt et ipsa facere! Tempore in adipisci</p> <p>nesciunt possimus ipsam doloribus vero enim, dolorem nemo iusto sed alias saepe soluta ducimus minus sint architecto eaque accusamus exercitationem quidem necessitatibus asperiores! Temporibus sapiente molestias eum illum itaque autem nostrum debitis officia quis? Quia animi cum, dicta doloremque architecto vel recusandae deleniti adipisci asperiores odio dignissimos veritatis voluptatem libero nulla sunt possimus. Harum aperiam inventore minima consequuntur distinctio corporis iure deserunt officia incidunt?</p> <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quis veniam quod nobis temporibus consequatur incidunt, impedit maxime totam minus officiis quia eveniet sed reprehenderit, nostrum ut fugit ipsam iusto accusantium tempora est. Laudantium suscipit sit doloribus dolore tenetur! Sapiente, ab?</p>',
+            'category_id' => 2, 
+            'user_id' => 1 
+        ]);
+         
+
     }
 }
--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -4,12 +4,13 @@
 @section('section')
     <h1 class="mb-5">Halaman Blog Posts</h1>
     @foreach($posts as $post)
+    
         <article class="mb-5 border-bottom pb-4">
             {{-- Inputan slug yang ada dalam link href ini berfungsi untuk mengisi slug yang nanti diambil oleh halamam single post --}}
             <a class="text-decoration-none" href="/post/{{ $post->slug }}"><h2>{{ $post->title }}</h2> </a>
             <p>By Virgi Septian in <a class="text-decoration-none" href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
             <p>{!! $post['excerpt'] !!}</p>
-            <p><a class="text-decoration-none" href="text-decoration-none" href="/post/{{ $post->slug }}">read more ...</a></p>
+            <p><a class="text-decoration-none" href="/post/{{ $post->slug }}">read more ...</a></p>
         </article>
     @endforeach
 @endsection
